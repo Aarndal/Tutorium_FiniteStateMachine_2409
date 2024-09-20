09f5ec4e62626e9946541139d90482a15bb5e011
Tony <acavaleri@outlook.de>
2024-09-20 13:41:11 +0100

    story verfasst

---
+++ Story.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//Würfel: Mond, Sternschnuppe, Schaf, Flugzeug, Fallschirm, Pyramide, Skarabeus, Maske, Zeit
+
+int Story() {
+    std::cout << "Während einer romantischen Nacht fiel eine Sternschnuppe auf ein Schaf." << std::endl;
+    std::cout << "Zur gleichen Zeit sprang ein Mann von einem Flugzeug mit seinem Fallschirm ab." << std::endl;
+    std::cout << "Als der Mann auf dem Boden landete, sah er eine Pyramide und versteckte sich in der Kammer des Pharaos." << std::endl;
+    std::cout << "Schützend hielt er einen magischen Skarabäus vor sich in der Hand." << std::endl;
+    std::cout << "Die Zeit verging und als die Gefahr gebannt war, kam der Mann wieder aus der Pyramide heraus." << std::endl;
+    std::cout << "Dabei trug er eine geheimnisvolle Maske, geklaut aus der Kammer des Pharao und verschwand in der Dunkelheit." << std::endl;
+
+    return 0;
+}
